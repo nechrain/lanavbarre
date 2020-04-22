0e10c4a2784f82175cb44a7b46a310a5797015b2
nessrine <nessrine.chammakhi.simplon@gmail.com>
2020-04-22 14:20:06 +0100

    nessrin

---
+++ src/header/List/List.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./List.css";
+
+function List() {
+  return (
+     
+    <ul>
+     
+      <li>
+        <a href="#">Accueil</a>
+      </li>
+      <li>
+        <a href="#">Equipe</a>
+      </li>
+      <li>
+        <a href="#">A Propos</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  );
+}
+
+export default List;
